6993a6f66a31d9f821602096b31b29678773bcf0
Brotaku <bnathanrobinson@gmail.com>
2023-07-20 13:59:14 -0400

    data persistence commit
    - UIMenuManager script for buttons to move user between scenes
    - renamed MainManager to GameManager
    - created new MainManager script for data persistence between scenes and consistency in tutorial

--- Assets/Scripts/MainManager.cs
+++ Assets/Scripts/MainManager.cs
@@ -1,76 +1,74 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using UnityEngine;
-using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class MainManager : MonoBehaviour
 {
-    public Brick BrickPrefab;
-    public int LineCount = 6;
-    public Rigidbody Ball;
+    public static MainManager Instance;
 
-    public Text ScoreText;
-    public GameObject GameOverText;
-    
-    private bool m_Started = false;
-    private int m_Points;
-    
-    private bool m_GameOver = false;
+    // VARIABLES FOR CURRENT SESSION
+    public string playerName;
+    public int currentScore;
 
-    
-    // Start is called before the first frame update
-    void Start()
+    // VARIABLES FOR HIGH SCORE SESSION
+    public int highScore;
+    public string highScoreName;
+
+    private void Awake()
     {
-        const float step = 0.6f;
-        int perLine = Mathf.FloorToInt(4.0f / step);
-        
-        int[] pointCountArray = new [] {1,1,2,2,5,5};
-        for (int i = 0; i < LineCount; ++i)
+        // destroys copy MainManager is one exists in scene with original
+        if (Instance != null)
         {
-            for (int x = 0; x < perLine; ++x)
-            {
-                Vector3 position = new Vector3(-1.5f + step * x, 2.5f + i * 0.3f, 0);
-                var brick = Instantiate(BrickPrefab, position, Quaternion.identity);
-                brick.PointValue = pointCountArray[i];
-                brick.onDestroyed.AddListener(AddPoint);
-            }
+            Destroy(gameObject);
+            return;
         }
+
+        // if this is the only MainManager, do not destroy
+        Instance = this;
+        DontDestroyOnLoad(gameObject);
+
+        LoadHighScore();
+        //Do this or the code won't know a high score exists in MainManager
     }
 
-    private void Update()
+    // data persistence across scenes
+    [System.Serializable]
+    public class SaveData
     {
-        if (!m_Started)
-        {
-            if (Input.GetKeyDown(KeyCode.Space))
-            {
-                m_Started = true;
-                float randomDirection = Random.Range(-1.0f, 1.0f);
-                Vector3 forceDir = new Vector3(randomDirection, 1, 0);
-                forceDir.Normalize();
-
-                Ball.transform.SetParent(null);
-                Ball.AddForce(forceDir * 2.0f, ForceMode.VelocityChange);
-            }
-        }
-        else if (m_GameOver)
-        {
-            if (Input.GetKeyDown(KeyCode.Space))
-            {
-                SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
-            }
-        }
+        public int highScore;
+        public string highScoreName;
     }
 
-    void AddPoint(int point)
+    public void SaveHighScore(int currentScore, string playerName)
     {
-        m_Points += point;
-        ScoreText.text = $"Score : {m_Points}";
+        //First, create a new instance of the save data
+        SaveData data = new SaveData();
+
+        //Then, specify what you want to store
+        data.highScore = currentScore;
+        data.highScoreName = playerName;
+
+        // Next, transform that instance to JSON with JsonUtility.ToJson;
+        string json = JsonUtility.ToJson(data);
+
+        // Finally, use the special method File.WriteAllText to write a string to file
+        File.WriteAllText(Application.persistentDataPath + "/savefile.json", json);
     }
 
-    public void GameOver()
+    public void LoadHighScore()
     {
-        m_GameOver = true;
-        GameOverText.SetActive(true);
+        // Find JSON save data and reassign it to its variable using JsonUtility.FromJson<T>(json);
+        string path = Application.persistentDataPath + "/savefile.json";
+        if(File.Exists(path))
+        {
+            string json = File.ReadAllText(path);
+            SaveData data = JsonUtility.FromJson<SaveData>(json);
+
+            // assign highScore and highScoreName its appropriate saved data
+            highScore = data.highScore;
+            highScoreName = data.highScoreName;
+        }
     }
 }
